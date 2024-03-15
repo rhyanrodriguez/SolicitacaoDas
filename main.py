5c59382076962abf1fb99f61d38a883f26780d21
rhyanrodriguez <87545357+rhyanrodriguez@users.noreply.github.com>
2024-03-15 14:28:17 -0300

    Locator Include

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from playwright.sync_api import sync_playwright, Playwright
+from time import sleep
 
 def onvioAutomation(p: Playwright): 
 
     browser = p.chromium.launch(headless=False)
     context = browser.new_context(accept_downloads=True)
     page = context.new_page()
-    page.goto("https://onvio.com.br/br-portal-do-cliente/service-requesting/general")
+    page.goto("https://onvio.com.br/#/")
     page.click("#trauth-continue-signin-btn")
     page.fill("#username","clara@talst.com.br")
     page.click('//button[contains(text(), "Entrar")]')
@@ -14,22 +15,28 @@ def onvioAutomation(p: Playwright):
     page.click('#mfa-reminder-close')
     page.click('#bm-header-app-menu-toggle')
 
-    
     #inserção dos dados para solicitação
 
-    page.click ('#client-combobox-5-input-5-input-5-input-5-input-5-input-5-input-5-input-5-input-5-input')
-    page.fill ('#client-combobox-5-input-5-input-5-input-5-input-5-input-5-input-5-input-5-input-5-input', '1428')
-    page.click ('//div[contains(text(), "MORETO CONTABILIDADE S/S LTDA")]')
-    page.click('#requester')
-    page.click('#bui-combobox-list-3-grid-cell-0x0')
-    page.click('//label[contains(text(), "OUTRO")]')
-    page.click('#department')
-    page.click('//div[contains(text(), "Departamento do Sistema")]')
-    page.fill('#subject', "Assunto teste")
-    page.fill('#description', "Lorem Ipsum")
-    page.fill('#due-date', "15032024")
+    with page.expect_popup() as pi:
+        page.goto("https://onvio.com.br/br-portal-do-cliente/service-requesting/general")
+    popup = (pi.value)
+
+    sleep (50)
+
+
+    page.locator('text=Adicionar').click()
+    popup.fill('#client-combobox-5-input-5-input-5-input-5-input-5-input-5-input-5-input-5-input-5-input', '1428')
+    popup.click('//div[contains(text(), "MORETO CONTABILIDADE S/S LTDA")]')
+    popup.click('#requester')
+    popup.click('#bui-combobox-list-3-grid-cell-0x0')
+    popup.click('//label[contains(text(), "OUTRO")]')
+    popup.click('#department')
+    popup.click('//div[contains(text(), "Departamento do Sistema")]')
+    popup.fill('#subject', "Assunto teste")
+    popup.fill('#description', "Lorem Ipsum")
+    popup.fill('#due-date', "15032024")
 
     
-    with sync_playwright as p:
-        onvioAutomation(p)
+with sync_playwright() as p:
+    onvioAutomation(p)
     
